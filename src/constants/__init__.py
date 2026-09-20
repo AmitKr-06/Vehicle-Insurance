e99d21b3f1de9f06d6d25d302dcb23121a91bbf2
Amit Kr <amiitaimlengg@gmail.com>
2026-09-20 09:53:04 +0530

    CICD test -1

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -67,7 +67,7 @@ MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 Model Evaluation related constants.
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "myfirst-model-mlopsproj"
+MODEL_BUCKET_NAME = "vehicle-insurance-model-registry"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
